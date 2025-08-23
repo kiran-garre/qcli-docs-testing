82ba82bfdc89c7263caa84f9353f88cc4cd19f08
kiran-garre <kiranbug@amazon.com>
2025-08-22 17:47:41 -0700

    Fixed quotes for gh pr jq

--- docs-generation/read-pr.sh
+++ docs-generation/read-pr.sh
@@ -5,7 +5,7 @@ PR_NUMBER=$1
 
 # Add PR information
 echo "====== PR Information ======\n" > $PR_FILE
-gh pr view $PR_NUMBER --json title,body --jq "Title: " + .title + "\nDescription: " + .body >> $PR_FILE
+gh pr view $PR_NUMBER --json title,body --jq '"Title: " + .title + "\nDescription: " + .body' >> $PR_FILE
 
 # Include updated files
 echo -e "\n====== Updated files ======\n" >> $PR_FILE
